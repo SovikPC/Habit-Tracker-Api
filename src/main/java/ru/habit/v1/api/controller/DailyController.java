b021ef16882608f4d66450bedf11b9e55332629f
SovikPC <vital.gurov2017@gmail.com>
2025-12-16 20:00:50 +0800

    Создание контроллеров API

---
+++ src/main/java/ru/habit/v1/api/controller/DailyController.java
@@ -0,0 +1,16 @@
+package ru.habit.v1.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("v1/daily")
+public class DailyController {
+
+    // @GetMapping()
+    // public List<DailyDto> getAllDaily() {
+    //     return map.mapListDailyDto(repository.findAll());
+    // }
+    
+}
