4998f0c0c1cea7e504f706cd1793eb719225605c
SovikPC <vital.gurov2017@gmail.com>
2025-12-21 18:08:47 +0800

    изменение контороллеров

---
+++ src/main/java/ru/habit/v1/api/controller/TagController.java
@@ -0,0 +1,66 @@
+package ru.habit.v1.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.habit.v1.api.model.Result;
+import ru.habit.v1.api.model.dto.TagDto;
+import ru.habit.v1.api.service.TagService;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("v1/tag/")
+public class TagController {
+
+    @Autowired
+    private TagService service;
+
+    @GetMapping("all")
+    public ResponseEntity<Result<List<TagDto>>> getAllTags() {
+        try{
+            return new ResponseEntity<>(service.findAllTag(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<TagDto>> getByIdTag(@PathVariable("id") UUID id) {
+        return new ResponseEntity<>(service.findByIdTag(id), HttpStatus.OK);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<TagDto> postTag(@RequestBody TagDto dto) {
+        service.createTag(dto);
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TagDto> putTag(@PathVariable UUID id, @RequestBody TagDto dto) {
+        service.updateTag(id,dto);
+        return new ResponseEntity<TagDto>(dto, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<> deleteTag(@PathVariable UUID id) {
+        service.deleteTag(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+}
---
+++ src/main/java/ru/habit/v1/api/controller/TaskController.java
@@ -0,0 +1,50 @@
+package ru.habit.v1.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.habit.v1.api.model.Result;
+import ru.habit.v1.api.model.dto.TaskDto;
+import ru.habit.v1.api.service.TaskService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@RestController
+@RequestMapping("v1/task/")
+public class TaskController {
+
+    @Autowired
+    private TaskService service;
+
+    @GetMapping("all")
+    public ResponseEntity<Result<List<TaskDto>>> getAllTask() {
+        return new ResponseEntity<>(service.findAllTask(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<TaskDto>> getByIdTask(@PathVariable("id") UUID id) {
+        return new ResponseEntity<>(service.findByIdTask(id), HttpStatus.OK);
+    }
+    
+
+    @PostMapping("create")
+    public ResponseEntity<TaskDto> postTask(@RequestBody TaskDto dto) {
+        service.createTask(dto);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+    
+
+}
