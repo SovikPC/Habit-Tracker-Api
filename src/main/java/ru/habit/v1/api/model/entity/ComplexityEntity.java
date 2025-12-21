3db98bf6fb3e598310ea17e3b1ddbeb7b151cf8a
SovikPC <vital.gurov2017@gmail.com>
2025-12-22 00:23:39 +0800

    Модификация Entity

--- src/main/java/ru/habit/v1/api/model/entity/ComplexityEntity.java
+++ src/main/java/ru/habit/v1/api/model/entity/ComplexityEntity.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "Complexity")
+@NoArgsConstructor
 public class ComplexityEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
